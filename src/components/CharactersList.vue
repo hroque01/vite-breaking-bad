2e8b3e1ebf1919832b07a268d2af6fcc08dc83d8
Hadson Roque <hadson.roque01@git config --global user.email hadson.roque01@gmail.com>
2022-12-15 23:43:11 +0800

    Creating Character list and Add to App.vue

---
+++ src/components/CharactersList.vue
@@ -0,0 +1,32 @@
+<script>
+import { store } from '../store.js';
+
+export default {
+    name: "CharacterList",
+    data() {
+        return {
+            store,
+        }
+    }
+}
+</script>
+
+<template>
+    <section class="container">
+        <div class="row">
+            <div>
+                <!-- template di card singola -->
+                <div class="text-center">
+                    <img src="" alt="">
+                    <h4>nome</h4>
+                    <div>stato</div>
+                    <div>specie</div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
--- src/store.js
+++ src/store.js
@@ -2,5 +2,8 @@ import { reactive } from 'vue';
 
 export const store = reactive({
     characterList: [],
-    apiUrl: 'https://rickandmortyapi.com/api/character'
+    apiUrl: 'https://rickandmortyapi.com/api/character',
+    titolo: "Rick and Morty App",
+
+
 })
