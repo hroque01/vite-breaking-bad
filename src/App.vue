a9ec9bd957c7e3963d480432ccd1a381f1eb7636
Hadson Roque <hadson.roque01@git config --global user.email hadson.roque01@gmail.com>
2022-12-15 23:25:41 +0800

    Add Header

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <script>
+import AppHeader from './components/AppHeader.vue';
+import { store } from './store.js';
 
+export default {
+  name: App,
+  components: {
+    AppHeader,
+  },
+  data() {
+    return {
+      store,
+    }
+  }
+}
 </script>
 
 <template>
-
+  <AppHeader msg="Rick and Morty App" />
 </template>
 
 <style lang="scss" scoped>
